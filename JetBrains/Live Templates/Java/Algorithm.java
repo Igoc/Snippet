3d63809f888d2f73e62258d61eab65f19cf7b1be
Igoc <zx0342@gmail.com>
2025-05-08 17:07:30 +0900

    Add JetBrains live templates for solving algorithm problem using Java

---
+++ JetBrains/Live Templates/Java/Algorithm.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class $CLASS_NAME$ {
+    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        in.close();
+        out.flush();
+        out.close();
+    }
+}
